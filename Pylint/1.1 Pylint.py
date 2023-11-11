c40703fb8b583469bb186ea6d7a84ff21dcbf613
Felipe, 俳画 <ainfanteh@unal.edu.co>
2023-11-11 14:15:46 -0500

    Add files via upload

---
+++ Pylint/1.1 Pylint.py
@@ -0,0 +1,12 @@
+def funcion():
+    numero = 500
+    print(Numero)
+
+funcion()
+# pylint "1.1 Pylint.py" -r y
+# +-------------+------------+
+# |message id   |occurrences |
+# +=============+============+
+# |invalid-name |1           |
+# +-------------+------------+
+
